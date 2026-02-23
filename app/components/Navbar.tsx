27b37d1a19a9932dff9d32692cc3478bc8abc2da
Rishikesh <DarnuleRishikesh@gmail.com>
2026-02-23 19:27:48 +0530

    added project and contact page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,32 +1,57 @@
 "use client";
+
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="navbar h-[3rem] w-fit z-1 ml-5  rounded-2xl flex items-center justify-center">
       <fieldset className="relative w-fit h-full flex  items-center justify-center ">
-        <input type="radio" id="Home" name="group" className="List-1 hidden" defaultChecked/>
-        <label
-          htmlFor="Home"
-          className="List-1 w-[8rem] duration-200 ease-out text-[#d2d2d2] z-1 font-[550] p-[0.4rem_2.2rem] flex items-center justify-center cursor-pointer"
-        >
-          Home
-        </label>
 
-        <input type="radio" id="Projects" name="group" className="List-2 hidden" />
-        <label
-          htmlFor="Projects"
-          className="List-2 w-[8rem] z-1 font-[550] duration-200 ease-out  text-[#bbbaba] cursor-pointer p-[0.4rem_2.2rem] flex items-center justify-center"
-        >
-          Projects
-        </label>
+        <Link href="/" >
+          <input
+            type="radio"
+            id="Home"
+            name="group"
+            className="List-1 hidden"
+            defaultChecked
+          />
+          <label
+            htmlFor="Home"
+            className="List-1 w-[8rem] duration-200 ease-out text-[#d2d2d2] z-1 font-[550] p-[0.4rem_2.2rem] flex items-center justify-center cursor-pointer"
+          >
+            Home
+          </label>
+        </Link>
 
-        <input type="radio" id="Contact" name="group" className="List-3 hidden" />
-        <label
-          htmlFor="Contact"
-          className="List-3 w-[8rem] z-1 font-[550] duration-200 ease-out text-[#bbbaba] cursor-pointer p-[0.4rem_2.2rem] flex items-center justify-center"
-        >
-          Contact
-        </label>
+        <Link href="/projects">
+          <input
+            type="radio"
+            id="Projects"
+            name="group"
+            className="List-2 hidden"
+          />
+          <label
+            htmlFor="Projects"
+            className="List-2 w-[8rem] z-1 font-[550] duration-200 ease-out  text-[#bbbaba] cursor-pointer p-[0.4rem_2.2rem] flex items-center justify-center"
+          >
+            Projects
+          </label>
+        </Link>
 
+        <Link href="/contact">
+          <input
+            type="radio"
+            id="Contact"
+            name="group"
+            className="List-3 hidden"
+          />
+          <label
+            htmlFor="Contact"
+            className="List-3 w-[8rem] z-1 font-[550] duration-200 ease-out text-[#bbbaba] cursor-pointer p-[0.4rem_2.2rem] flex items-center justify-center"
+          >
+            Contact
+          </label>
+        </Link>
         <div className="Highlighter"></div>
       </fieldset>
     </nav>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col justify-around items-center `}
       >
         {children}
       </body>
