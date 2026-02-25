d5d35d957dd7d9a4c66d7ccc04d4c422b814cd45
Rishikesh <DarnuleRishikesh@gmail.com>
2026-02-25 20:37:17 +0530

    minor UI changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,7 +12,9 @@ export default function Navbar() {
     { id: 56, name: "Projects", path: "/projects" },
     { id: 98, name: "Contact", path: "/contact" },
   ];
-  const selectedPath = navLinks.findIndex((li) =>  path === li.path.split("/")[1] )
+  const selectedPath = navLinks.findIndex(
+    (li) => path === li.path.split("/")[1],
+  );
 
   const handleClick = (idx: number) => {
     HighlighterRef.current?.style.setProperty("--pos", `${idx * 100}% `);
@@ -26,14 +28,22 @@ export default function Navbar() {
             key={li.id}
             href={`${li.path}`}
             onClick={() => handleClick(idx)}
-            onLoad={() => HighlighterRef.current?.style.setProperty("--pos", `${idx * 100}%`)}
-            className={`w-[8rem]  text-[#d2d2d2] z-1 font-[550] p-[0.4rem_2.2rem] flex items-center justify-center cursor-pointer ${path === li.path.split("/")[1] ? "text-black" : "text-[#d2d2d2]"}`}
+            onLoad={() =>
+              HighlighterRef.current?.style.setProperty(
+                "--pos",
+                `${idx * 100}%`,
+              )
+            }
+            className={`w-[8rem]  text-[#888686] z-1 font-[550] p-[0.4rem_2.2rem] flex items-center justify-center cursor-pointer ${path === li.path.split("/")[1] ? "text-[#ddd9d9]" : "text-[#888686]"}`}
           >
             {li.name}
           </Link>
         ))}
 
-        <div className="Highlighter" style={{transform:`translateX(${selectedPath * 100}%)`}}></div>
+        <div
+          className="Highlighter"
+          style={{ transform: `translateX(${selectedPath * 100}%)` }}
+        ></div>
       </fieldset>
     </nav>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Image from "next/image";
+import Link from "next/link";
 
 export default function page() {
   const Projects = [
@@ -7,7 +10,12 @@ export default function page() {
       name: "Aurastream",
       img: "/project/Aurastream.png",
       desc: "An anime website",
-      stack: ["/Icons/nextjs.svg", "/Icons/typescript.svg", "/Icons/redux.svg",  "/Icons/tailwindcss.svg"],
+      stack: [
+        "/Icons/nextjs.svg",
+        "/Icons/typescript.svg",
+        "/Icons/redux.svg",
+        "/Icons/tailwindcss.svg",
+      ],
       demo: "https://aurastreams.vercel.app",
       github: "https://github.com/Rishikesh2704/AnimeApp",
     },
@@ -16,7 +24,11 @@ export default function page() {
       name: "Recili",
       img: "/project/Recili.png",
       desc: "A recipe website",
-      stack: ["/Icons/nextjs.svg", "/Icons/typescript.svg", "/Icons/tailwindcss.svg"],
+      stack: [
+        "/Icons/nextjs.svg",
+        "/Icons/typescript.svg",
+        "/Icons/tailwindcss.svg",
+      ],
       demo: "https://recili.vercel.app",
       github: "https://github.com/Rishikesh2704/Recipes-App",
     },
@@ -26,7 +38,9 @@ export default function page() {
       {Projects.map((project) => (
         <div
           key={project.id}
-          className="ProjectCard h-[15rem] w-[25rem] relative bg-black rounded-lg flex items-center justify-center"
+          className="ProjectCard h-[15rem] w-[25rem] relative bg-black rounded-lg flex items-center justify-center cursor-pointer "
+          onMouseOver={(e) => e.currentTarget.style.setProperty('--glow','30px')}
+          onMouseOut={(e) => e.currentTarget.style.setProperty('--glow','10px')}
         >
           <figure className="text-center h-full rounded-lg">
             <Image
@@ -36,15 +50,40 @@ export default function page() {
               alt={project.name + "website image"}
               className="h-auto w-full rounded-lg"
             />
-            <figcaption className="h-[7.5rem] absolute bottom-0 w-full rounded-b-lg Decription flex flex-col justify-around items-center text-center text-white">
-                <h1 className="absolute bottom-4 text-xl font-semibold  text-[#aa00ff]">{project.name}</h1>
+            <figcaption className="h-[7.5rem] absolute bottom-0 w-full rounded-b-lg Decription flex flex-col justify-center pl-4 items-start text-center text-white">
+              <h1 className="TextBg  text-xl font-semibold  text-[#aa00ff]">
+                {project.name}
+              </h1>
+              <desc className="text-gray-300">{project.desc}</desc>
+              <div className="absolute bottom-2 flex gap-5 justify-center items-center">
+                <Link
+                  href={project.demo}
+                  className="text-[1rem] text-gray-300 hover:text-gray-500"
+                  target="/"
+                >
+                  Live
+                </Link>
+                <Link
+                  href={project.github}
+                  className="text-[1rem] text-gray-300 hover:text-gray-500"
+                  target="/"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </Link>
+              </div>
             </figcaption>
           </figure>
           <div className="absolute top-0 right-0 flex gap-1 text-gray-300 w-[fit] h-[1.5rem] pl-2 pr-2 rounded-tr-lg bg-black rounded-l-lg">
             {project.stack.map((frame, idx) => (
-                <Image key={idx + frame} src={frame} height={20} width={20} alt="framework"/>
+              <Image
+                key={idx + frame}
+                src={frame}
+                height={20}
+                width={20}
+                alt="framework"
+              />
             ))}
-            </div>
+          </div>
         </div>
       ))}
     </section>
